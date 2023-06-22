72177860ebd1107034a44960cdcffb2810b36aa0
gaurangkudale <gaurangkudale@yahoo.com>
2023-06-23 05:02:00 +0530

    added loop code

--- loops/main.go
+++ loops/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	// "github.com/gogo/protobuf/test/indeximport-issue72/index"
+)
 
 func main() {
 	fmt.Println("Welcome to Loops in golang")
+
+	days := []string{"Sunday", "Monday", "Tuesday","Wednesday","Thusrsday","Friday","Saturday"}
+	fmt.Println(days)
+
+	//For Loop type 1
+
+	// for d := 0; d < len(days); d++ {
+	// 	fmt.Println(days[d])
+	// }
+
+	// For loop type 2
+
+	// for i := range days{
+	// 	fmt.Println(days[i])
+	// }
+
+	// For Loop type 3 
+	
+	// for index, day := range days{
+	// 	fmt.Printf("For index %v the value is %v \n", index, day)
+	// }
+
+	
 }
