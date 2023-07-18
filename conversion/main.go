9bb27a2e69d0a653ad8da64106f4d8ec8c43bb18
gaurangkudale <gaurangkudale@yahoo.com>
2023-07-19 02:46:14 +0530

    changes

--- conversion/main.go
+++ conversion/main.go
@@ -4,35 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
+	// "strconv"
+	// "strings"
 )
 
 func main() {
 	// fmt.Println("welcome to our pizza app")
 	// fmt.Println("Please rate our pizza between 1 to 5 ")
 
-
 	// reader := bufio.NewReader(os.Stdin)
 	// input, _ := reader.ReadString('\n')
 
 	// fmt.Println("Thanks for ratting, ", input)
 
-	// NumRating, err := strconv.ParseFloat(strings.TrimSpace(input),64) 
+	// NumRating, err := strconv.ParseFloat(strings.TrimSpace(input),64)
 	// if err != nil {
 	// 	fmt.Println(err)
 	// } else{
-	// 	fmt.Println("Added 1 to your rating: ", NumRating + 1)  
+	// 	fmt.Println("Added 1 to your rating: ", NumRating + 1)
 	// }
 
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
-	for input.Scan(){
+	for input.Scan() {
 		counts[input.Text()]++
 	}
-	for line, n := range counts{
-		if n>1{
-			fmt.Printf("%d\t%s\n",n,line)
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 
